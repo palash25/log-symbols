d81d5ba6a6c56c6f54ce6def7dada4d09e29d677
Palash Nigam <npalash25@gmail.com>
2018-12-11 11:23:36 +0530

    Add symbols.rb

--- lib/symbols.rb
+++ lib/symbols.rb
@@ -1,8 +1,55 @@
 require "symbols/version"
+require "colorize"
 
-module Log
-  module Symbols
-    class Error < StandardError; end
-    # Your code goes here...
+# These symbols are only available on unix
+MAIN_SYMBOLS = {
+    :info => 'ℹ',
+    :success => '✔',
+    :warning => '⚠',
+    :error => '✖'
+}
+
+# These sumbols are only used in case of windows
+# since windows doesn't support the MAIN_SYMBOLS
+FALLBACK_SYMBOLS = {
+    :info => '¡',
+    :success => 'v',
+    :warning => '!!',
+    :error => '×'
+}
+
+def main_supported
+    if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) == nil
+        return true
+    else
+        return false
+    end
+end
+
+module SYMBOLS
+  class Log
+    def initialize
+        @SYMBOL = if main_supported
+                    MAIN_SYMBOLS
+                  else
+                    FALLBACK_SYMBOLS
+                  end     
+    end
+    
+    def info
+        @SYMBOL[:info].colorize(:blue)
+    end
+
+    def success
+        @SYMBOL[:success].colorize(:green)
+    end
+
+    def warning
+        @SYMBOL[:warning].colorize(:yellow)
+    end
+
+    def error
+        @SYMBOL[:error].colorize(:red)
+    end
   end
 end
